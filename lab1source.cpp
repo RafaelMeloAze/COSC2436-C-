ad12c980e48b36444907f6a889d10735471c10b2
RafaelMeloAze <35881228+RafaelMeloAze@users.noreply.github.com>
2018-01-28 13:06:50 -0600

    Add files via upload

--- lab1source.cpp
+++ lab1source.cpp
@@ -33,9 +33,13 @@ ofstream outfile;
 
 
 //--------------------------PROTOTYPES
-void askUser();
-void power();
-double powerMath();
+void askUser(double&, int&);
+double dpower(double, int);
+double powerMath(double, int, double, int);
+
+
+
+
 
 
 
@@ -44,12 +48,41 @@ double powerMath();
 
 int main()
 {
-
+	//------------------------------------VARIABLES
+	double base = 0;
+	int power = 1;
+	int again = 1;
 	
 
 
 	//------------------------------------FUNCTION CALLS
-	askUser();
+	
+	while (again == 1)
+	{
+		system("CLS");
+ 
+	askUser(base, power);
+	cout << "The total is : " << dpower(base, power) << endl;
+
+
+
+	//-----------------------------------------ASK TO RUN AGAIN
+	cout << "\n Would like to try again ? (Type 1 for YES or 2 For NO) : ";
+	cin >> again;
+	//-------------CHECK IF INPUT IS VALID
+	while (again < 1 || again > 2 || cin.fail())
+	{
+		cout << "\nPlease check your entry. What you entered is not a valid." << endl;
+		cout << "\n Would like to try again ? (Type 1 for YES or 2 For NO) : ";
+		cin.clear();
+		cin.ignore(2000, '\n');
+		cin >> again;
+	}
+	//-----------------------------------------ASK TO RUN AGAIN
+
+
+	}
+
 
 
 
@@ -72,15 +105,15 @@ int main()
 
 
 //====================================================USER INTERFACE------------------------
-void askUser()
+void askUser(double& base, int& power)
 {
-	double base;
-	int power;
+	
 	cout << "This program will allow you to power any real number. \n" << endl;
 	cout << "Please enter your base number:";
 	cin >> base;
 
-	while (cin.fail())
+	//-------------CHECK IF INPUT IS VALID
+	while (cin.fail())   
 	{
 		cout << "\nPlease check your entry. What you entered is not a valid number." << endl;
 		cout << "You must enter any integer number." << endl;
@@ -94,6 +127,7 @@ void askUser()
 	cout << "Please enter your power number:";
 	cin >> power;
 
+	//-------------CHECK IF INPUT IS VALID
 	while (power < 0 || cin.fail())
 	{
 		cout << "\nPlease check your entry. What you entered is not a positive whole number." << endl;
@@ -101,13 +135,11 @@ void askUser()
 		cout << "Please enter your base number:";
 		cin.clear();
 		cin.ignore(2000, '\n');
-		cin >> base;
+		cin >> power;
 
 	}
 
 	
-
-
 }
 
 //****************************************************END USERINTERFACE--------------------
@@ -115,19 +147,28 @@ void askUser()
 
 
 //====================================================POWER FUNCTION------------------------
-void power()
+double dpower(double base, int power)
 {
+	int times = 2;
+    double total = base * base;
+		
+	return powerMath(base, power, total, times);
 
 }
 //****************************************************END POWER FUNCTION--------------------
 
 
 //====================================================POWER MATH FUNCTION------------------------
-double powerMath()
+double powerMath(double base, int power, double total, int times)
 {
-	double total = 1;
-	
-
+	if(times == power)
 	return total;
+	
+	total = total * base;
+	++times;
+
+	return 	powerMath(base, power, total, times);
 }
 //****************************************************END MATH FUNCTION--------------------
+
+
